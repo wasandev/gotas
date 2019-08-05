a68c367e0f036bcdc34b433c70e69cbb2d850086
Wasan Ongart <o_wasan@hotmail.com>
2019-08-05 13:46:18 +0700

    change helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,9 +1,9 @@
 <?php
 
-if (! function_exists('tenant_fqdn')) {
+if (!function_exists('tenant_fqdn')) {
     /**
      * Generates a fully qualified domain name for a tenant from a given subdomain.
-     * 
+     *
      * @param  string $subdomain
      * @return string
      */
