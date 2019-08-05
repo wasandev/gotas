7f6d8f4b05d7d5ee0197449111c10a5a002a45ed
Wasan Ongart <o_wasan@hotmail.com>
2019-08-05 16:02:43 +0700

    update app layout resource

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,26 +34,26 @@
         @endif
 
 
-        {{--
-        @if (App::environment('production', 'staging'))
-            <script>
-                if ('serviceWorker' in navigator) {
-                    window.addEventListener('load', function() {
-                        navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                        // Registration was successful
-                        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                        }, function(err) {
-                        // registration failed :(
-                        console.log('ServiceWorker registration failed: ', err);
-                        });
-                    });
-                    }
-            </script>
-        @endif
+
+        // @if (App::environment('production', 'staging'))
+        //     <script>
+        //         if ('serviceWorker' in navigator) {
+        //             window.addEventListener('load', function() {
+        //                 navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        //                 // Registration was successful
+        //                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        //                 }, function(err) {
+        //                 // registration failed :(
+        //                 console.log('ServiceWorker registration failed: ', err);
+        //                 });
+        //             });
+        //             }
+        //     </script>
+        // @endif
 
 
-        <link rel ="manifest" href ="/manifest.json">
-    --}}
+        // <link rel ="manifest" href ="/manifest.json">
+
 
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
         @stack('scripts')
