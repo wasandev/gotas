fe46d3af4f8f26bce2be8f2257b4b59fce561634
Wasan Ongart <o_wasan@hotmail.com>
2019-08-09 13:38:59 +0700

    update tenents route

--- routes/tenants.php
+++ routes/tenants.php
@@ -3,8 +3,9 @@ Route::middleware('web')
     ->namespace('\\App\\Http\\Controllers\Tenant\\')
     ->group(function () {
 
-        Route::get('/', 'tenantController@index')->name('index');
+        Route::get('/', 'TenantController@index')->name('index');
     });
+
 
 Nova::routes()
     ->withAuthenticationRoutes()
