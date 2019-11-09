16abc5ef4ecc0e7e82bbd076e3e7168c53de15d9
Wasan Ongart <o_wasan@hotmail.com>
2019-11-09 16:37:43 +0700

    change logo

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -1,2 +1,2 @@
 {{-- <span class=" text-grey-darkest text-center text-2xl  font-extrabold">GOTAS</span> --}}
-<img class="w-24  mx-auto text-center" src="{{ Storage::url('gotaslogo.png') }}" alt="">
+<img class="w-32  mx-auto text-center" src="{{ Storage::url('gotaslogo.png') }}" alt="">
